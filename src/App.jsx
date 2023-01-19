58ddf53f5cedd0016ca0c118bd71b69ea8238d44
aji <wiji@atop.com.tw>
2023-01-19 15:57:11 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Chart1 from "./Chart1";
 function App() {
   const [DI, setDI] = useState({ address: null, value: null });
   const [DO, setDO] = useState({ address: null, value: null });
+  const [AI, setAI] = useState({ address: null, value: null });
 
   const rD = useRef();
   const wD = useRef();
+  const rA = useRef();
 
   const readDigital = async e => {
     e.preventDefault();
@@ -59,6 +61,29 @@ function App() {
     setDO(state => ({ address, value }));
   };
 
+  const readAnalog = async e => {
+    e.preventDefault();
+    let address = parseInt(rD.current[0].value);
+
+    let responseData;
+
+    try {
+      const response = await fetch("http://localhost:8888/readanalog", {
+        method: "POST",
+        body: JSON.stringify({ address }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      responseData = await response.json();
+    } catch (err) {
+      console.error(err);
+    }
+
+    console.log(responseData);
+    setAI(state => ({ address, value: responseData.value }));
+  };
+
   return (
     <div
       className='App'
@@ -66,30 +91,48 @@ function App() {
         height: "100vh",
       }}
     >
-      <form ref={rD} onSubmit={e => readDigital(e)}>
-        <h2>Read Digital</h2>
-        <input type='number' />
+      <h2>Read Digital</h2>
+      <form ref={rD} onSubmit={readDigital}>
+        <input type='number' name='address' placeholder='address' />
         <button>submit</button>
       </form>
-
       <div>
         Value in address {DI.address === null ? "kosong" : DI.address} ={" "}
         {DI.value === null ? "kosong" : DI.value}
       </div>
 
-      <form ref={wD} onSubmit={e => writeDigital(e)}>
-        <h2>Write Digital</h2>
+      <h2>Write Digital</h2>
+      <form ref={wD} onSubmit={writeDigital}>
         <input type='number' name='address' placeholder='address' />
         <input type='number' name='value' placeholder='value' />
         <button>submit</button>
       </form>
-
       <div>
         Value in address {DO.address === null ? "kosong" : DO.address} ={" "}
         {DO.value === null ? "kosong" : DO.value}
       </div>
 
-      {/* <Chart1 /> */}
+      <h2>Read Analog</h2>
+      <form ref={rD} onSubmit={readAnalog}>
+        <input type='number' name='address' placeholder='address' />
+        <button>submit</button>
+      </form>
+      <div>
+        Value in address {AI.address === null ? "kosong" : AI.address} ={" "}
+        {AI.value === null ? "kosong" : AI.value}
+      </div>
+
+      <h2>Write Analog</h2>
+      <form ref={rD} onSubmit={readAnalog}>
+        <input type='number' name='address' placeholder='address' />
+        <button>submit</button>
+      </form>
+      <div>
+        Value in address {AI.address === null ? "kosong" : AI.address} ={" "}
+        {AI.value === null ? "kosong" : AI.value}
+      </div>
+
+      <Chart1 />
     </div>
   );
 }
